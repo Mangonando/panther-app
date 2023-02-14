5a31b18f60a46f46ccddae35b58a20a0b0307861
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2023-02-14 15:34:03 +0100

    Initial Commit

---
+++ Panther/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Panther
+//
+//  Created by Juan Ramirez on 14.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Panther/PantherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantherApp.swift
+//  Panther
+//
+//  Created by Juan Ramirez on 14.02.23.
+//
+
+import SwiftUI
+
+@main
+struct PantherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
